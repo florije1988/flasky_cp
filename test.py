ab7fc15d4f27aff55e30645978f2822a701d71e4
florije1988 <florije1988@gmail.com>
2014-08-19 10:13:35 +0800

    添加一小部分内容。

--- test.py
+++ test.py
@@ -1,9 +1,25 @@
 # -*- coding: utf-8 -*-
 __author__ = 'florije'
 
+
+def decorate(func):
+    print("in decorate function, decorating", func.__name__)
+
+    def wrapper_func(*args):
+        print("Executing", func.__name__)
+        return func(*args)
+    return wrapper_func
+
+
+def myfunction(parameter):
+    print(parameter)
+
+
 if __name__ == '__main__':
-    dicts = {'1': 1, '2': 2}
-    print sorted(dicts.iteritems(), key=lambda dicts: dicts[1], reverse=True)
+    # dicts = {'1': 1, '2': 2}
+    # print sorted(dicts.iteritems(), key=lambda dicts: dicts[1], reverse=True)
 
+    testfunction = decorate(myfunction)
+    print testfunction
 
-
+    print testfunction("hello")
