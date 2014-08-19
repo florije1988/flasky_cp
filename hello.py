a667901258e4b93d1870d9774635ee8753c357d5
florije1988 <florije1988@gmail.com>
2014-08-19 17:24:50 +0800

    Flask-SQLAlchemy

--- hello.py
+++ hello.py
@@ -4,6 +4,8 @@ __author__ = 'florije'
 from flask import Flask, render_template, session, redirect, url_for, flash
 from flask.ext.bootstrap import Bootstrap
 from flask.ext.moment import Moment
+import os
+from flask.ext.sqlalchemy import SQLAlchemy
 from datetime import datetime
 
 from flask.ext.wtf import Form
@@ -15,6 +17,31 @@ app.config['SECRET_KEY'] = 'fuboqing'
 
 bootstrap = Bootstrap(app)
 moment = Moment(app)
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
+
+db = SQLAlchemy(app)
+
+
+class Role(db.Model):
+    __tablename__ = 'roles'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), unique=True)
+
+    def __repr__(self):
+        return '<Role {name}>'.format(self.name)
+
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), unique=True, index=True)
+
+    def __repr__(self):
+        return '<User {username}>'.format(self.username)
 
 
 # @app.route('/index')
