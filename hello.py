11bd8dbbfa39483c482100d1259e73ee4b4be7ba
florije1988 <florije1988@gmail.com>
2014-08-31 20:04:16 +0800

    完成第四章的内容。

--- hello.py
+++ hello.py
@@ -120,17 +120,39 @@ def internal_server_error(e):
 #     return render_template('index_cp.html', form=form, name=session.get('name'))
 
 
+# @app.route('/', methods=['GET', 'POST'])
+# def index():
+#     form = NameForm()
+#     if form.validate_on_submit():
+#         old_name = session.get('name')
+#         if old_name is not None and old_name != form.name.data:
+#             flash('Looks like you have changed your name!')
+#         session['name'] = form.name.data
+#         form.name.data = ''
+#         return redirect(url_for('index'))
+#     return render_template('index_cp.html', form=form, name=session.get('name'))
+
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
     form = NameForm()
     if form.validate_on_submit():
-        old_name = session.get('name')
-        if old_name is not None and old_name != form.name.data:
-            flash('Looks like you have changed your name!')
+        user = User.query.filter_by(username=form.name.data).first()
+        if user is None:
+            user = User(username=form.name.data)
+            db.session.add(user)
+            session['known'] = False
+        else:
+            session['known'] = True
         session['name'] = form.name.data
         form.name.data = ''
         return redirect(url_for('index'))
-    return render_template('index_cp.html', form=form, name=session.get('name'))
+    return render_template('index_rp.html', form=form, name=session.get('name'), known=session.get('known', False))
+
+@app.route('/init', methods=['GET'])
+def init():
+    db.create_all()
+    return 'ok'
 
 
 class NameForm(Form):
@@ -140,4 +162,5 @@ class NameForm(Form):
 if __name__ == '__main__':
     # app.run(debug=True)
     app.debug = True
-    manager.run()
+    # manager.run()
+    app.run()
