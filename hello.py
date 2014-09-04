a0d21775fe02fb8ca7a1c420f8a7c6452de6c1b2
florije1988 <florije1988@gmail.com>
2014-09-04 17:54:30 +0800

    第六章内容

--- hello.py
+++ hello.py
@@ -19,6 +19,7 @@ from flask.ext.migrate import Migrate, MigrateCommand
 import os
 
 from flask.ext.mail import Mail
+from flask.ext.mail import Message
 
 app = Flask(__name__)
 manager = Manager(app)
@@ -29,6 +30,10 @@ app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME')
 app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD')
+
+app.config['FLASKY_MAIL_SUBJECT_PREFIX'] = '[Flasky]'
+app.config['FLASKY_MAIL_SENDER'] = 'Flasky Admin <flasky@example.com>'
+app.config['FLASKY_ADMIN'] = os.environ.get('FLASKY_ADMIN')
 
 mail = Mail(app)
 
@@ -44,6 +49,7 @@ db = SQLAlchemy(app)
 
 migrate = Migrate(app, db)
 manager.add_command('db', MigrateCommand)
+
 
 class Role(db.Model):
     __tablename__ = 'roles'
@@ -67,15 +73,14 @@ class User(db.Model):
 
 # @app.route('/index')
 # def index():
-#     mydict = {'key': 'fuboqing'}
-#     mylist = [1, 2, 3, 4]
+# mydict = {'key': 'fuboqing'}
+# mylist = [1, 2, 3, 4]
 #     myintvar = 2
 #     myobj = MyObj()
 #     return render_template('index.html', mydict=mydict, mylist=mylist, myintvar=myintvar, myobj=myobj)
 
 
 class MyObj(object):
-
     def somemethod(self):
         return 'somemethod'
 
@@ -89,7 +94,6 @@ class Comment(object):
 
 
 class User_Cp(object):
-
     def __repr__(self):
         return "I am User!"
 
@@ -149,6 +153,23 @@ def internal_server_error(e):
 #     return render_template('index_cp.html', form=form, name=session.get('name'))
 
 
+# @app.route('/', methods=['GET', 'POST'])
+# def index():
+#     form = NameForm()
+#     if form.validate_on_submit():
+#         user = User.query.filter_by(username=form.name.data).first()
+#         if user is None:
+#             user = User(username=form.name.data)
+#             db.session.add(user)
+#             session['known'] = False
+#         else:
+#             session['known'] = True
+#         session['name'] = form.name.data
+#         form.name.data = ''
+#         return redirect(url_for('index'))
+#     return render_template('index_rp.html', form=form, name=session.get('name'), known=session.get('known', False))
+
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
     form = NameForm()
@@ -158,12 +179,17 @@ def index():
             user = User(username=form.name.data)
             db.session.add(user)
             session['known'] = False
+            if app.config['FLASKY_ADMIN']:
+                send_email(app.config['FLASKY_ADMIN'], 'New User',
+                           'mail/new_user', user=user)
         else:
             session['known'] = True
         session['name'] = form.name.data
         form.name.data = ''
         return redirect(url_for('index'))
-    return render_template('index_rp.html', form=form, name=session.get('name'), known=session.get('known', False))
+    return render_template('index.html', form=form, name=session.get('name'),
+                           known=session.get('known', False))
+
 
 @app.route('/init', methods=['GET'])
 def init():
@@ -174,12 +200,22 @@ def init():
 def make_shell_context():
     return dict(app=app, db=db, User=User, Role=Role)
 
+
 manager.add_command("shell", Shell(make_context=make_shell_context()))
+
+
+def send_email(to, subject, template, **kwargs):
+    msg = Message(app.config['FLASKY_MAIL_SUBJECT_PREFIX'] + subject,
+                  sender=app.config['FLASKY_MAIL_SENDER'], recipients=[to])
+    msg.body = render_template(template + '.txt', **kwargs)
+    msg.html = render_template(template + '.html', **kwargs)
+    mail.send(msg)
 
 
 class NameForm(Form):
     name = StringField('What is your name?', validators=[DataRequired()])
     submit = SubmitField('Submit')
+
 
 if __name__ == '__main__':
     # app.run(debug=True)
