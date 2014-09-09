4e3272446cb6f1895e63dddb13fafd0bfc0a1729
florije1988 <florije1988@gmail.com>
2014-09-09 16:33:52 +0800

    初始化配置文件config.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,13 @@
 # -*- coding: utf-8 -*-
+import config
+
 __author__ = 'florije'
 
-from flask import Flask, render_template
+from flask import Flask
 from flask.ext.bootstrap import Bootstrap
 from flask.ext.mail import Mail
 from flask.ext.moment import Moment
 from flask.ext.sqlalchemy import SQLAlchemy
-from config import config
 
 
 bootstrap = Bootstrap()
@@ -26,5 +27,7 @@ def create_app(config_name):
     db.init_app(app)
 
     # attach routes and custom error pages here
+    from main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
 
     return app
--- app/main/forms.py
+++ app/main/forms.py
@@ -1,2 +1,10 @@
 # -*- coding: utf-8 -*-
 __author__ = 'florije'
+from flask.ext.wtf import Form
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+
+
+class NameForm(Form):
+    name = StringField('What is your name?', validators=[DataRequired()])
+    submit = SubmitField('Submit')
